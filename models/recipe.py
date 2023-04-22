1284510be7377019938c2f05bd0811e12c15a225
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2023-04-23 00:05:46 +0300

    recipe-model: add label and ingredient attributes for more clarity in the recipe

--- models/recipe.py
+++ models/recipe.py
@@ -1,14 +1,27 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 
+class Ingredient(BaseModel):
+    text: str
+    quantity: float
+    measure: Optional[str]
+    food: str
+    weight: float
+    image: Optional[str]
+
+
 class Recipe(BaseModel):
+    label: str
+    source: str
     image: str
     ingredientLines: List[str]
+    ingredients: List[Ingredient]
     calories: float
     cuisineType: List[str]
     url: str
+    totalTime: float
 
 
 class RecipeResponse(BaseModel):
